15464b5f7ca63c9a64bb2da188c0a0070e0f498b
Eduardo Troncoso <eduardujuca@outlook.com>
2023-10-11 14:54:38 -0300

    retornar cada pesquisa da pag

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,3 +1,7 @@
 <?php
 $filename = "file:///C:/Users/eduar/Downloads/exercicios-2023-master/exercicios-2023-master/php/assets/origin.html";
 $content = file_get_contents($filename);
+
+#encontrar cada pesquisa na página
+if (preg_match_all('/<a href="https:\/\/proceedings.science\/proceedings\/100227\/_papers\/.*?" class="paper-card p-lg bd-gradient-left">.*?<\/a>/', $content, $matches)) {
+}
