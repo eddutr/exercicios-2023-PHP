68c9a75417ce3136f50f223d31eb1395041a7a1a
Eduardo Troncoso <eduardujuca@outlook.com>
2023-10-11 14:55:40 -0300

    retornar titulo da pesquisa

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -4,4 +4,15 @@ $content = file_get_contents($filename);
 
 #encontrar cada pesquisa na página
 if (preg_match_all('/<a href="https:\/\/proceedings.science\/proceedings\/100227\/_papers\/.*?" class="paper-card p-lg bd-gradient-left">.*?<\/a>/', $content, $matches)) {
+
+    foreach ($matches[0] as $match) {
+        $titulo = '';
+        $id = '';
+        $tipo = '';
+
+        #retornar titulo
+        if (preg_match_all('/<h4 class="my-xs paper-title">(.*?)<\/h4>/', $match, $matchTitle)) {
+            $titulo = $matchTitle[1];;
+        }
+    }
 }
